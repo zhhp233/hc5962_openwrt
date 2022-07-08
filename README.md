4de31b2831220e2afef8a392d839f211c8b15039
ZZZ <741788184@qq.com>
2022-07-08 17:20:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Actions-OpenWrt for P3TERX
+
+## 感谢P3TERX大佬的教程
+
+[P3TERX的Github地址](https://github.com/P3TERX/Actions-OpenWrt) 
+
+
+注意事项：
+.config里面搜索并注释掉naiveproxy
---
+++ diy-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
+
+# 去除默认主题
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#rm -rf package/lean/luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-ifit/' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-edge/' feeds/luci/collections/luci/Makefile
